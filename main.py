26bd0352e6f157b5f2a56589f4a054bc544b6274
Sanad67 <kgzn0742@leeds.ac.uk>
2026-06-13 22:20:43 +0400

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+p = requests.get(url)
+data = p.json()
+price = data["bitcoin"]["usd"]
+print(f"The price is: {price}")
