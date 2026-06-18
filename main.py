54c9670874358afbc71f7f64c1eeb2b4044c009c
Sanad67 <kgzn0742@leeds.ac.uk>
2026-06-18 13:22:07 +0400

    fetch and structure 7-day BTC price history

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import requests
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-p = requests.get(url)
-data = p.json()
-price = data["bitcoin"]["usd"]
-print(f"The price is: ${price}")
+#Before running ensure you are in the venv before activating 
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+#Then activate venv\Scripts\Activate.ps1  
+
+import requests , json
+import pandas as pd
+
+url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7"
+resp = requests.get(url)
+data = resp.json()
+
+prices = data["prices"]
+
+df = pd.DataFrame(prices)
+
+df.rename(columns={0: 'Timestamp', 1: 'Price'}, inplace=True)
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit = 'ms')
+df = df.set_index('Timestamp')
+print(df)
+
+
+
+
+
+
+
