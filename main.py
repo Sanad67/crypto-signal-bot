ef9a7238048a22ccf53ff3575405ba0fa497a92e
Sanad67 <kgzn0742@leeds.ac.uk>
2026-06-19 20:42:27 +0400

    add MA3 rolling average column

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ resp = requests.get(url)
 data = resp.json()
 
 prices = data["prices"]
-
 df = pd.DataFrame(prices)
-
+                    
 df.rename(columns={0: 'Timestamp', 1: 'Price'}, inplace=True)
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit = 'ms')
 df = df.set_index('Timestamp')
+df["MA3"] = df['Price'].rolling(window = 3).mean()
 print(df)
 
 
