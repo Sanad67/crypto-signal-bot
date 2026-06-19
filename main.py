c0d517f3d34520b8ab7a08029b4aadad996c924a
Sanad67 <kgzn0742@leeds.ac.uk>
2026-06-19 23:46:22 +0400

    Add MA crossover BUY/SELL signal detection

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 
 import requests , json
 import pandas as pd
+import numpy as np
 
-url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7"
+url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=90"
 resp = requests.get(url)
 data = resp.json()
 
@@ -16,7 +17,17 @@ df.rename(columns={0: 'Timestamp', 1: 'Price'}, inplace=True)
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit = 'ms')
 df = df.set_index('Timestamp')
 df["MA3"] = df['Price'].rolling(window = 3).mean()
-print(df)
+df['MA10'] = df['Price'].rolling(window=10).mean()
+
+df['price_diff'] = df['MA3'] - df['MA10']
+df['diff_prev'] = df['price_diff'].shift(1)
+
+
+df['Signal'] = ''
+df.loc[(df['diff_prev'] <= 0) & (df['price_diff'] > 0), 'Signal'] = 'BUY'
+df.loc[(df['diff_prev'] >= 0) & (df['price_diff'] < 0), 'Signal'] = 'SELL'
+
+print(print(df[df['Signal'] != '']))
 
 
 
